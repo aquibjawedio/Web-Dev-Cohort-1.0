ad07220204d68e92212389f7cd29839af47d0873
Aquib Jawed <aquibjawed.dev@gmail.com>
2025-02-19 16:34:50 +0530

    BMI Index Project DONE

---
+++ projects/03-bmi-index/script.js
@@ -0,0 +1,19 @@
+const submit = document.querySelector('.submit');
+
+submit.addEventListener('click', () => {
+    let userHeight = document.querySelector('.user-height').value;
+    let userWeight = document.querySelector('.user-weight').value;
+
+    if (!userHeight.trim() || !userWeight.trim()) return alert('Enter valid input');
+
+    userHeight = parseInt(userHeight);
+    userWeight = parseFloat(userWeight);
+
+    if (userHeight.toString() == 'NaN' || userWeight.toString() == 'NaN') return alert('Enter valid input');
+
+
+    document.querySelector('.show-bmi-index').textContent = (userWeight / (userHeight * userHeight * 0.0001)).toFixed(2);
+
+    document.querySelector('.user-height').value = "";
+    document.querySelector('.user-weight').value = "";
+})
